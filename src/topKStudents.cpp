00ce74fe3bd7a34da14234395efb35c92662bcdf
sankethchalla <sankethchalla@missionrnd.com>
2016-03-18 23:35:27 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -21,6 +21,36 @@ struct student {
 	int score;
 };
 
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+void sort(struct student *students, int len)
+{
+	int i, j;
+	struct student temp;
+	for (i = 0; i < len - 1; ++i)
+	{
+		for (j = 0; j < len - i - 1; ++j)
+		{
+			if (students[j].score < students[j + 1].score)
+			{
+				temp = students[j];
+				students[j] = students[j + 1];
+				students[j + 1] = temp;
+			}
+		}
+	}
+}
+
+struct student ** topKStudents(struct student *students, int len, int k) {
+	struct student **a;
+	int i, j = 0;
+	if ( k <= 0)
+		return NULL;
+	a = (struct student **)malloc(k*sizeof(struct student));
+	sort(students, len);
+	if (k > len)
+		k = len;
+	for (i = 0; i<k; i++)
+	{
+		a[i] = &students[i];
+	}
+	return a;
 }
